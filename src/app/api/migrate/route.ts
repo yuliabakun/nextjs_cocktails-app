abbb7a5ef46f22b1d2daecfbf7d90b409b411366
Yuliia Bakun <bakunyuliia@gmail.com>
2024-02-07 18:21:14 +0100

    fixed types issue

--- src/app/api/migrate/route.ts
+++ src/app/api/migrate/route.ts
@@ -13,45 +13,6 @@ export async function GET() {
     }
   }
 
-  function modifyData(data) {
-    const modifiedCocktails = data.map(cocktail => {
-      const modifiedCocktail = {
-        id: cocktail.idDrink,
-        drink: cocktail.strDrink,
-        drinkThumb: cocktail.strDrinkThumb,
-        imageSource: cocktail.strImageSource,
-        drinkAlternate: cocktail.strDrinkAlternate,
-        tags: cocktail.strTags,
-        category: cocktail.strCategory,
-        iba: cocktail.strIBA,
-        alcoholic: cocktail.strAlcoholic,
-        glass: cocktail.strGlass,
-        instructions: cocktail.strInstructions,
-        ingredients: [],
-      };
-
-
-      for (let i = 1; i <= 15; i++) {
-        const ingredient = cocktail[`strIngredient${i}`];
-        const measurement = cocktail[`strMeasure${i}`];
-
-        if (ingredient && measurement) {
-          const [value, measureType] = measurement.trim().split(' ');
-
-          modifiedCocktail.ingredients.push({
-            ingredient,
-            measurement: value,
-            measureType,
-          });
-        }
-      }
-
-      return modifiedCocktail;
-    })
-
-    return modifiedCocktails;
-  }
-
   const cocktails = readData();
 
   return NextResponse.json({ cocktails }, { status: 200 });
