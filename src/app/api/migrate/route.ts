af1d57360e7e580ad3dbf460f5e82f9aeffe35c3
Yuliia Bakun <bakunyuliia@gmail.com>
2024-02-08 10:46:32 +0100

    added env to vercel

--- src/app/api/migrate/route.ts
+++ src/app/api/migrate/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse } from "next/server";
 import fs from 'fs';
 
+//util route
+
 export async function GET() {
   const pathToFile = 'E:/julia/projects/pet/nextjs_cocktails-app/public/cocktailsData.json';
 
