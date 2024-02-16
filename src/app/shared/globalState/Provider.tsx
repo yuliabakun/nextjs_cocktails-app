461d50664ffe4778b7f8bbc15ac6941c45265eb2
Yuliia Bakun <bakunyuliia@gmail.com>
2024-02-16 13:25:10 +0100

    added props to provider

--- src/app/shared/globalState/Provider.tsx
+++ src/app/shared/globalState/Provider.tsx
@@ -1,8 +1,13 @@
 'use client';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { ReactNode } from 'react';
 
-export function Providers({ children }) {
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
   return (
     <Provider store={store}>
       {children}
